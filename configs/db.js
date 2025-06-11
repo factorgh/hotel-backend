34632b67b2ed90883ba5bec19e1db6abdc5171e8
factorgh <burchellsbale@gmail.com>
2025-06-11 15:34:31 +0000

    UpDATE ON NAMINGS

--- configs/db.js
+++ configs/db.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    try {
-        mongoose.connection.on('connected', () => console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`);
-    } catch (error) {
-        console.error(error.message);
-    }
-
+  try {
+    mongoose.connection.on("connected", () =>
+      console.log("Database Connected")
+    );
+    await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`, {
+      bufferCommands: false,
+      connectTimeoutMS: 20000,
+      socketTimeoutMS: 45000,
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 export default connectDB;
